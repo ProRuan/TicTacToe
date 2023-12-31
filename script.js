47f721ed0d7c92c5216a640474f332cbacc2edfa
Your Name <you@example.com>
2023-12-31 22:56:55 +0100

    added render()

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let fields = [
     null,
-    null,
-    null,
+    'circle',
+    'cross',
     null,
     null,
     null,
@@ -14,3 +14,30 @@ let fields = [
 function init() {
     render();
 }
+
+
+function render() {
+    const content = document.getElementById('content');
+
+    let table = '<table>';
+    for (let i = 0; i < 3; i++) {
+        table += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            const fieldId = i * 3 + j;
+            let symbol = '';
+            if (fields[fieldId] === 'circle') {
+                symbol = 'o';
+            } else if (fields[fieldId] === 'cross') {
+                symbol = 'x';
+            }
+            table += `<td>${symbol}</td>`;
+        }
+        table += '</tr>';
+    }
+    table += `</table>`;
+
+    content.innerHTML = table;
+}
+
+
+// eventuell HTML code auslagern und einfach Variablen einfuegen
